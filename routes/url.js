da1bd308921e9ac96fd599aa13a7bf9f42d20292
aayu17 <nextcoders.89@gmail.com>
2024-11-04 14:22:45 +0530

    change in routes

--- routes/url.js
+++ routes/url.js
@@ -3,5 +3,6 @@ const { generateNewShortURL,handleGetAnalytics } = require("../controllers/url")
 const router = express.Router();
 
 router.post("/", generateNewShortURL);
+router.post("/", generateNewShortUR);
 router.get('/analytics/:shortId',handleGetAnalytics)
 module.exports = router;
